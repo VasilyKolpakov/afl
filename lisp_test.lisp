aebcc18c8d030ce2140193d07ff1dac86f201277
Vasily Kolpakov <vasilykolpakov@gmail.com>
2021-07-07 23:13:20 +0300

    lisp tests, div operator fix

---
+++ lisp_test.lisp
@@ -0,0 +1,36 @@
+; import lisp_interpreter.2s
+
+"arithmetic"
+(+ 1 2)
+(* 2 2)
+(- 3 1)
+(+ (* 2 2) (* 1 1))
+(/ 4 2)
+
+"lambda"
+(((lambda (x) (lambda (y) (+ x y))) 1) 2)
+((lambda (f s) f) 1 2)
+
+"if"
+(if true 1 2)
+(if false 1 2)
+
+"factorial"
+(define factorial (lambda (n)
+    (if (> n 0)
+        (* (factorial (- n 1)) n)
+        1)))
+(factorial 5)
+
+"equal? numbers"
+(equal? 1 1)
+(equal? 1 0)
+
+"equal? lists"
+(equal? (list 1 2 3) (list 1 2 3))
+(equal? (list 1 3) (list 1 2 3))
+
+"quote"
+(quote (lambda (x) (+ x x)))
+
+(exit 0)
---
+++ run_file_non_interactive.sh
@@ -0,0 +1,10 @@
+function cat_imports {
+    ./list_imports.sh "$1" |                # list all imports with duplicates in dependency-first sort order
+    awk '{printf("%04d\t%s\n", NR, $0)}' |  # prepend each line with a padded line number, e.g. 0042
+    sort -k 2 |                             # sort by file name
+    uniq -f 1 |                             # remove duplicate names
+    sort |                                  # sort by original line number
+    cut -f 2 |                              # remove line numbers
+    xargs cat /dev/null                     # cat all files
+}
+(cat_imports $1 ; cat $1) | ./run_boot.sh 
