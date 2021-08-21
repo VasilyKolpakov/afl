ac2b1040cd96e09de6a7f82325737724b3a548dc
Vasily Kolpakov <vasilykolpakov@gmail.com>
2021-08-21 11:43:42 +0300

    macro system

--- core.lisp
+++ core.lisp
@@ -10,22 +10,80 @@
       nil
       (cons (f (car l)) (map f (cdr l))))))
 
-(define transform-let-expr (lambda (let-expr)
+(define assert
+  (lambda (v p)
+    (if (p v)
+      v
+      (begin
+        (print-string "assertion failed: ")
+        (print p)
+        (print-string " on ")
+        (println v)
+        (print-stack-trace)
+        (exit 1)))))
+
+(define alist-lookup-rec
+  (lambda (l k)
+    (if (equal? l nil)
+      nil
+      (if (equal? (car (car l)) k)
+        (cdr (car l))
+        (alist-lookup-rec (cdr l) k)))))
+
+(define alist-lookup
+  (lambda (l k) (alist-lookup-rec (assert l list?) k)))
+
+(define transform-let-expr (lambda (let-expr-args)
     (cons
         (cons (quote lambda)
               (list
-                (map (lambda (binding) (car binding)) (car (cdr let-expr)))
-                (car (cdr (cdr let-expr)))))
-        (map (lambda (binding) (car (cdr binding))) (car (cdr let-expr))))))
+                (map (lambda (binding) (car binding)) (car let-expr-args))
+                (car (cdr let-expr-args))))
+        (map (lambda (binding) (car (cdr binding))) (car let-expr-args)))))
 
 
 (define transform-all-let-exprs (lambda (expr)
     (if (list? expr)
       (map transform-all-let-exprs
-           (if (equal? (car expr) (quote let))
-             (transform-let-expr expr)
+           (if (equal? (car expr) 'let)
+             (transform-let-expr (cdr expr))
              expr))
       expr)))
+
+(eval (transform-all-let-exprs '(define fixed-point-with-limit
+  (lambda (f l i)
+    (if (< l 1)
+      nil
+      (let ((fi (f i)))
+        (if (equal? i fi)
+          i
+          (fixed-point-with-limit f (- l 1) fi))))))))
+
+(define macro-list (cell nil))
+
+(define add-macro
+  (lambda (sym macro-func)
+    (cell-set macro-list (cons (cons sym macro-func) (cell-get macro-list)))))
+
+(eval (transform-all-let-exprs '(define apply-single-macro
+  (lambda (expr)
+    (let ((macro-func (alist-lookup (cell-get macro-list) (car expr))))
+      (if (equal? macro-func nil)
+        expr
+        (macro-func (cdr expr))))))))
+
+(eval (transform-all-let-exprs '(define apply-all-macro
+  (lambda (expr)
+    (if (list? expr)
+      (let ((newexpr (map apply-all-macro expr)))
+        (apply-single-macro newexpr))
+      expr)))))
+
+(define macro-expand
+  (lambda (expr)
+    (fixed-point-with-limit apply-all-macro 10 expr)))
+
+(add-macro 'let transform-let-expr)
 
 (define REPL-print-enabled (cell false))
 
@@ -40,10 +98,21 @@
 
 (define REPL (lambda ()
     (begin
-      (REPL-print-value (eval (transform-all-let-exprs (read-syntax))))
+      (REPL-print-value (eval (macro-expand (read-syntax))))
       (REPL))))
 
 (REPL)
+
+(define extract-defun-lambda
+  (lambda (expr-args)
+    (let ((args (cdr (car expr-args)))
+          (body (car (cdr expr-args))))
+      (list 'lambda args body))))
+
+(add-macro 'defun
+           (lambda (expr-args)
+             (let ((lam (extract-defun-lambda expr-args)))
+               (list 'define (car (car expr-args)) lam))))
 
 
 (define syscall-mmap 9)
