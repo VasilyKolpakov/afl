56ccc0e229f9dd4de6387d4e5285bc3e6639c622
Vasily Kolpakov <vasilykolpakov@gmail.com>
2021-09-06 08:17:16 +0300

    run_lisp_tests: stash changes before tests

--- run_lisp_tests.sh
+++ run_lisp_tests.sh
@@ -4,6 +4,7 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
+git stash
 ./run_file_non_interactive.sh lisp_test.scm > lisp_test_actual_output
 if cmp lisp_test_expected_output lisp_test_actual_output ; then
     echo "${green}OK${reset}"
@@ -11,3 +12,4 @@ else
     echo "${red}FAIL${reset}"
     diff -y lisp_test_expected_output lisp_test_actual_output
 fi
+git stash pop
