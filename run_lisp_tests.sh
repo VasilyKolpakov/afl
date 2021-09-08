019173c2915c64602ae17e55a558dbc8c076206f
Vasily Kolpakov <vasilykolpakov@gmail.com>
2021-09-08 17:02:33 +0300

    remove stash\unstash from lisp test script

--- run_lisp_tests.sh
+++ run_lisp_tests.sh
@@ -4,7 +4,6 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-git stash
 ./run_file_non_interactive.sh lisp_test.scm > lisp_test_actual_output
 if cmp lisp_test_expected_output lisp_test_actual_output ; then
     echo "${green}OK${reset}"
@@ -12,4 +11,3 @@ else
     echo "${red}FAIL${reset}"
     diff -y lisp_test_expected_output lisp_test_actual_output
 fi
-git stash pop
