453c636fa20ec7ce2c315948c4ca8257738f4903
Vasily Kolpakov <vasilykolpakov@gmail.com>
2021-10-17 21:31:24 +0300

    codegen: labels, jmp instruction

--- codegen.scm
+++ codegen.scm
@@ -1,7 +1,7 @@
 (import "core.scm")
 
 (define (syscall-mmap-anon-exec size)
-  (syscall syscall-mmap 
+  (syscall syscall-mmap
            0 ; address hint
            size
            (bitwise-ior syscall-mmap-PROT-EXEC syscall-mmap-PROT-WRITE)
@@ -25,7 +25,7 @@
   (write-mem-byte (+ 1 ptr) #xc3) ; ret
   )
 
-(define return-instruction (list 2 generate-return))
+(define return-instruction (list 2 generate-return "ret"))
 
 (define (generate-add ptr)
   (write-mem-byte ptr #x58) ; pop rax
@@ -58,7 +58,7 @@
 (define (add-rsp-instruction value)
   (assert-stmt "value <= 127" (<= value 127))
   (assert-stmt "value >= 128" (>= value -128))
-  (list 4 (lambda (ptr) (generate-add-rsp ptr value)) "add-rsp"))
+  (list 4 (lambda (ptr) (generate-add-rsp ptr value)) (list "add-rsp" value)))
 ; 48 8b 45 ff
 
 (define (generate-push-var ptr index)
@@ -84,6 +84,21 @@
 
 (define set-frame-pointer-instruction
   (list 4 generate-set-frame-pointer "set-frame-pointer"))
+
+(define (generate-jmp ptr target)
+  (println target)
+  (write-mem-byte ptr #x48) ; mov rax, imm-value
+  (write-mem-byte (+ 1 ptr) #xb8)
+  (write-mem-i64  (+ 2 ptr) target)
+  (write-mem-byte (+ 10 ptr) #xff) ; jmp rax
+  (write-mem-byte (+ 11 ptr) #xe0)
+  )
+
+(define (jmp-instruction target-label)
+  (list 12
+        (lambda (ptr label-locs)
+          (generate-jmp ptr (alist-lookup label-locs target-label)))
+        (list "jmp" target-label)))
 
 
 (define (prefix-sum l)
@@ -125,8 +140,6 @@
           (else (panic "bad statement" stmt)))))
 
 
-(define fpointer (syscall-mmap-anon-exec 1000))
-(define buffer (syscall-mmap-anon 1000))
 
 (define (compile-procedure local-vars-with-inits statements)
   (let ((local-var-inits (map (lambda (v) (car (cdr v))) local-vars-with-inits))
@@ -141,23 +154,72 @@
         (add-rsp-instruction (* 8 (length local-vars)))
         return-instruction))))
 
+(define next-label-id (cell 0))
+
+(define (generate-label)
+  (let ((id (cell-get next-label-id)))
+    (cell-set next-label-id (+ id 1))
+    id))
+
+(define (label? v) (number? v))
+
+(define buffer (syscall-mmap-anon 1000))
+(define the-label (generate-label))
 (define instructions
+  (list
+    set-frame-pointer-instruction
+    (jmp-instruction the-label)
+    (push-imm-instruction buffer)
+    (push-imm-instruction 42)
+    set-64-instruction
+    the-label
+    return-instruction))
+
+(define instructions_
   (compile-procedure
-    '((a 1)
+    '(
+      (a 1)
       (b 2)
-      (c 3))
-    '((set-64 ,buffer (+ a (+ b c)))
-      (set-64 ,(+ 8 buffer) (+ 20 (+ a (+ b c)))))))
+      (c 3)
+      )
+    '(
+      (set-64 ,buffer (+ a (+ b c)))
+      (set-64 ,(+ 8 buffer) (+ a (+ b c)))
+      )))
 
-(foreach println instructions)
+(foreach
+  println
+  (map
+    (lambda (i)
+      (if (label? i)
+          (list 'label i)
+          (drop i 2)))
+    instructions))
 
-(let ((inst-locations (prefix-sum (map (lambda (x) (car x)) instructions)))
-      (locations-and-insts (zip inst-locations instructions)))
-  (foreach (lambda (loc-and-i) 
-             (let ((loc (car loc-and-i))
-                   (inst (cdr loc-and-i))
+(define (instruction-size i)
+  (if (label? i)
+      0
+      (car i)))
+
+(define fpointer (syscall-mmap-anon-exec 1000))
+
+(println fpointer)
+
+(let ((inst-locations (map
+                        (lambda (l) (+ l fpointer))
+                        (prefix-sum (map instruction-size instructions))))
+      (with-locations (zip instructions inst-locations))
+      (insts-and-locations (filter (lambda (x) (not (label? (car x)))) with-locations))
+      (labels-and-locations (filter (lambda (x) (label? (car x))) with-locations)))
+  (println labels-and-locations)
+  (foreach (lambda (i-and-loc)
+             (let ((inst (car i-and-loc))
+                   (loc (cdr i-and-loc))
                    (generator (car (cdr inst))))
-               (generator (+ fpointer loc)))) locations-and-insts))
+               (if (= (arity generator) 2)
+                   (generator loc labels-and-locations)
+                   (generator loc))))
+           insts-and-locations))
 
 (enable-REPL-print)
 (native-call fpointer)
